88a6461f2bf7dc5c5771b4ef784a42204e60bef8
kaleshripad11 <kaleshripad11@gmail.com>
2023-03-02 22:35:14 +0530

    Added one line to code

--- src/test/java/basicselenium/frames/AsgnFrame.java
+++ src/test/java/basicselenium/frames/AsgnFrame.java
@@ -1,6 +1,9 @@
 package basicselenium.frames;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -22,13 +25,17 @@ public class AsgnFrame {
 	}
 	
 	void openHerokuApp() {
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 		driver.get("https://the-internet.herokuapp.com/iframe");
 		inputFrame = driver.findElement(By.cssSelector("iframe.tox-edit-area__iframe"));
 		driver.switchTo().frame(inputFrame);
 		driver.findElement(By.cssSelector("body#tinymce")).clear();
 		driver.findElement(By.cssSelector("body#tinymce")).sendKeys(sampleString);
+		driver.findElement(By.cssSelector("body#tinymce")).sendKeys(Keys.CONTROL+"A");
+		driver.findElement(By.xpath("//*[name()='path' and contains(@d,'M7.8 19c-.')]")).click();
+		driver.findElement(By.xpath("//button[@title='Italic']//span[@class='tox-icon tox-tbtn__icon-wrap']//*[name()='svg']")).click();
 	}
-
+	
 	public static void main(String[] args) throws InterruptedException {
 		AsgnFrame assignment = new AsgnFrame(new FirefoxDriver());
 		assignment.openHerokuApp();
