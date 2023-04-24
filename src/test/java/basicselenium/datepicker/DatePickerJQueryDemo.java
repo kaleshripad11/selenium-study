40e923fc28ff3cd3fd41d96f8913e8a373551113
Shripad Kale <50066426+kaleshripad11@users.noreply.github.com>
2023-04-24 08:20:21 +0530

    Update DatePickerJQueryDemo.java
    Updated comments

--- src/test/java/basicselenium/datepicker/DatePickerJQueryDemo.java
+++ src/test/java/basicselenium/datepicker/DatePickerJQueryDemo.java
@@ -2,7 +2,14 @@ package basicselenium.datepicker;
 
 /*
 * Approaches
-* 1. Put date using sendkeys directly(not applicable always)
+* 1. Put date using sendkeys directly(not applicable
+* always)
+* 2. Selecting date dynamically at runtime
+* a. Locate datepicker and click
+* b. Get month, year and date in a variable and compare it with month, yr and date present in calendar
+* For this comparison, start a while loop and in it store  values for month and year on each iteration
+* inside loop at one if block and keep comparing date and month. 
+* After comparing the date and month, keep clicking next button for next month
 */
 
 public class DatePickerJQueryDemo {
