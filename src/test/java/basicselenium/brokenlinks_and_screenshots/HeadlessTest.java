eca05ffb88a3842c42f36c4363b28d82452f43c9
kaleshripad11 <kaleshripad11@gmail.com>
2023-07-01 19:51:28 +0530

    Updated package - data driven testing

--- src/test/java/basicselenium/brokenlinks_and_screenshots/HeadlessTest.java
+++ src/test/java/basicselenium/brokenlinks_and_screenshots/HeadlessTest.java
@@ -1,75 +1,38 @@
 package basicselenium.brokenlinks_and_screenshots;
-
-import java.io.File;
-import java.io.IOException;
-
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
-public class HeadlessTest {
-	/*
-	 * To perform headless test Options class can be used
-	 */
+class HeadlessTest{
 	
-	WebDriver driver;
-	public HeadlessTest() {
-		// TODO Auto-generated constructor stub
+	//@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+	
+		FirefoxOptions foptions = new FirefoxOptions();
+		foptions.addArguments("-headless");
 		
-	}
-	
-	void setupDriver() {
-		FirefoxOptions fOptions = new FirefoxOptions();
-		fOptions.addArguments("--headless");
-		driver = new FirefoxDriver();
-		driver.get("https://testautomationpractice.blogspot.com/");
+		//foptions.setHeadless(true);
+		WebDriver driver = new FirefoxDriver();
+		//WebPage Heading - "Automation Testing Practice" [xpath - "//div/h1[@class='title']"]
+		
+		//HeadlessTest headless = new HeadlessTest();
 		driver.manage().window().maximize();
-	}
-	
-	void getFullPageScreenshot() throws IOException {
-		TakesScreenshot screenshot = (TakesScreenshot)driver;
-	
-		// Define temp screenshot file type and store it to file variable
-		File source = screenshot.getScreenshotAs(OutputType.FILE);
+		driver.get("https://testautomationpractice.blogspot.com/");
 		
-		// Define the destination location to store the temp screenshot file
-		File destination = new File("/Users/shripadkale/Documents/Selenium_Java/selenium-study/screenshots/TestAutomationPractice.png");
+		//Validate page heading in Headless mode
 		
-		// Use FileUtils class to copy the temp file to the destination
-		FileUtils.copyFile(source, destination);
-	}
-	
-	void getPartialScreenshot() throws IOException {
-		/*
-		 * This feature is part of selenium 4 and newer versions
-		 * No need to use 'TakeScreenshot' interface to capture partial screenshots
-		 */
+		WebElement pageHeading = driver.findElement(By.xpath("//div/h1[@class='title']"));
+		String expectedHeading = "Automation Testing Practice";
 		
-		WebElement table = driver.findElement(By.xpath("//*[@id='HTML1']/div[1]/table"));
+		if(pageHeading.getText().equals(expectedHeading)) {
+			System.out.println("Expected heading and Actual heading are same! Hence test case passed!!");
+		}
+		else {
+			System.out.println("Expected heading and Actual heading are not matching! Hence test case failed!!");
+		}
 		
-		File src = table.getScreenshotAs(OutputType.FILE);
-		
-		File dest = new File("/Users/shripadkale/Documents/Selenium_Java/selenium-study/screenshots/TableOnly.png");
-		
-		FileUtils.copyFile(src, dest);
-	}
-	
-	
-	
-	
-	public static void main(String[] args) throws IOException  {
-		//FirefoxOptions options = new FirefoxOptions();
-		// options.setHeadless(true); //=> Deprecated method setHeadless();
-		//options.addArguments("-headless");
-		HeadlessTest headless = new HeadlessTest();
-		headless.setupDriver();
-		headless.getFullPageScreenshot();
-		headless.getPartialScreenshot();
-		headless.driver.quit();
+		driver.quit();
 	}
 }
--- src/test/java/datadriventesting/ReadDataFromXcel.java
+++ src/test/java/datadriventesting/ReadDataFromXcel.java
@@ -1,14 +1,60 @@
 package datadriventesting;
 
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class ReadDataFromXcel {
+	
+	/*
+	 * Use FileInputStream class to read the files(Package : java.io)
+	 * XSSFWorkbook => For dealing with Workbook
+	 * XSSSheet => Deal with Sheets in workbook
+	 * XSSFRow => Deal with rows in sheets
+	 * XSSFCell => Deal with cells in the sheets
+	 */
+	
+	void readFiles() throws IOException {
+		// Read xcel file using 'FileInputStream' class
+		FileInputStream readXcel = new FileInputStream(System.getProperty("user.dir")+"//data//TestDataHungamaTV.xlsx");
+		
+		// Read workbook
+		XSSFWorkbook workbook = new XSSFWorkbook(readXcel);
+		
+		// Read sheet
+		XSSFSheet  sheet = workbook.getSheet("Test Data");
+		
+		// Read columns and rows by getting cells
+		int rows = sheet.getLastRowNum();  // Will return total rows
+		int columns = sheet.getRow(1).getLastCellNum();		// Get total columns
+		
+		for(int i = 0;i<=rows;i++) {
+			// Outer loop - iterate through the rows
+			XSSFRow current_row = sheet.getRow(i); 	//Get current row in the sheet
+			
+			for(int j=0;j<columns;j++) {
+				// Inner loop - Iterate through the each single cell present on the row
+				XSSFCell current_cell = current_row.getCell(j);
+				//String cell_value = current_row.getCell(j).toString();
+				String current_value = current_cell.toString();
+				System.out.print(current_value+ "\t|\t");
+			}
+			
+			// Move pointer to next row to print next row
+			System.out.println();
+		}
+		workbook.close();
+		readXcel.close();
+	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		/*
-		 * - Use FileInputStream class to read the files(Package : java.io)
-		 * 
-		 */
-
+		ReadDataFromXcel xcel = new ReadDataFromXcel();
+		xcel.readFiles();
 	}
 
 }
