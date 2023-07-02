470a36c268829aad8ad6c3f353ddb64e782e301d
kaleshripad11 <kaleshripad11@gmail.com>
2023-07-02 21:34:36 +0530

    Updated file 'WriteDataInExcelFile.java'

--- src/test/java/datadriventesting/WriteDataInExcelFile.java
+++ src/test/java/datadriventesting/WriteDataInExcelFile.java
@@ -3,9 +3,7 @@ package datadriventesting;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Random;
-import java.util.stream.IntStream;
-
+import java.util.*;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -21,25 +19,46 @@ public class WriteDataInExcelFile {
 	 * 4. 
 	 */
 
-	void getInput() {
-		
+	FileOutputStream outputFile;
+	XSSFWorkbook testWorkbook;
+	XSSFSheet testSheet;
+	Scanner scan;
+
+	
+	void createFile(String filePath) throws FileNotFoundException {
+		// Create file  
+		outputFile = new FileOutputStream(filePath);
+				
+		// Create workbook 
+		testWorkbook = new XSSFWorkbook();
+				
+		// Create sheet
+		testSheet = testWorkbook.createSheet();
 	}
 	
 
 	
-	void writeDataToAFile() throws IOException, FileNotFoundException {
+	void writeDataToFile() throws IOException, FileNotFoundException {
 		
-		// Create file  
-		FileOutputStream outputFile = new FileOutputStream(System.getProperty("user.dir")+"//data//TestData.xlsx");
+		// Get cell input from a user to store in a cell 
+		String cell;
+
+		scan = new Scanner(System.in);
+		for(int i=0;i<3;i++) {
+			// Write rows
+			XSSFRow row = testSheet.createRow(i);
+			
+			for(int j=0;j<3;j++) {
+				// Write cells on each row
+				cell = scan.nextLine();
+				row.createCell(j).setCellValue(cell);
+			}
+			System.out.println("One row added");
+		}
 		
-		// Create workbook 
-		XSSFWorkbook testWorkbook = new XSSFWorkbook();
-		
-		// Create sheet
-		XSSFSheet testSheet = testWorkbook.createSheet();
 		
 		// First create row
-		XSSFRow r1 = testSheet.createRow(0);
+		/*
 		r1.createCell(0).setCellValue("Employee ID");
 		r1.createCell(1).setCellValue("Employee Name");
 		r1.createCell(2).setCellValue("Employee Salary");
@@ -53,7 +72,7 @@ public class WriteDataInExcelFile {
 		r3.createCell(0).setCellValue("EMP101");
 		r3.createCell(1).setCellValue("XZY");
 		r3.createCell(2).setCellValue("1300000");
-		
+		*/
 		// Once done with adding rows, add the file to workbook
 		testWorkbook.write(outputFile);
 		testWorkbook.close();
@@ -65,7 +84,12 @@ public class WriteDataInExcelFile {
 	public static void main(String[] args) throws IOException, FileNotFoundException  {
 		// TODO Auto-generated method stub
 		WriteDataInExcelFile writeXcelFile = new WriteDataInExcelFile();
-		writeXcelFile.writeDataToAFile();
+		
+		// Call to createFile(arg) function
+		writeXcelFile.createFile(System.getProperty("user.dir")+"//data//TestData.xlsx");
+		
+		// Call to writeDataToFile();
+		writeXcelFile.writeDataToFile();
 		//System.out.print();
 	}
 
