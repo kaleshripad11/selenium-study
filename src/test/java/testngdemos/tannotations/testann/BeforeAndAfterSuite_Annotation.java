7aa8c8e0f41f041bd5403fc50b4557e897a4396c
kaleshripad11 <kaleshripad11@gmail.com>
2023-09-16 21:48:37 +0530

    Updated class files

--- src/test/java/testngdemos/tannotations/testann/BeforeAndAfterSuite_Annotation.java
+++ src/test/java/testngdemos/tannotations/testann/BeforeAndAfterSuite_Annotation.java
@@ -1,9 +1,52 @@
 package testngdemos.tannotations.testann;
 
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class BeforeAndAfterSuite_Annotation {
 	/*
 	 * BeforeSuite & AfterSuite annotation will be executed only once before starting the suite & after completing the suite
 	 * 
 	 */
 
+	@BeforeSuite
+	void executeBeforeSuite() {
+		System.out.println("Before Suite");
+	}
+	
+	@AfterSuite
+	void executeAfterSuite() {
+		System.out.println("After Suite");
+	}
+	
+	@BeforeTest
+	void login() {
+		System.out.println("Logging in...");
+		System.out.println("Logged in successfully!!!\n");
+	}
+	
+	@AfterTest
+	void logout() {
+		System.out.println("Logging out...");
+		System.out.println("Logged out successfully!!!\n");
+	}
+	
+
+	@Test(priority=1)
+	void performProductSearch() {
+		System.out.println("Searching for mobile...\n");
+	}
+	
+	@Test(priority=2)
+	void openProductDetails() {
+		System.out.println("Loading mobile details...\n");
+	}
+	
+	@Test(priority=3)
+	void placeProductOrder() {
+		System.out.println("Order placed successfully\n");
+	}
 }
--- src/test/java/testngdemos/tannotations/testann/BeforeAndAfterTest_Annotation.java
+++ src/test/java/testngdemos/tannotations/testann/BeforeAndAfterTest_Annotation.java
@@ -1,4 +1,5 @@
 package testngdemos.tannotations.testann;
+import org.testng.annotations.*;
 
 public class BeforeAndAfterTest_Annotation {
 	/*
@@ -6,4 +7,32 @@ public class BeforeAndAfterTest_Annotation {
 	 * Will be executed every time before starting tests & after ending the tests
 	 */
 
+	@BeforeTest
+	void login() {
+		System.out.println("Logging in...");
+		System.out.println("Logged in successfully!!!\n");
+	}
+	
+	@AfterTest
+	void logout() {
+		System.out.println("Logging out...");
+		System.out.println("Logged out successfully!!!\n");
+	}
+	
+
+	@Test(priority=1)
+	void performProductSearch() {
+		System.out.println("Searching for mobile...\n");
+	}
+	
+	@Test(priority=2)
+	void openProductDetails() {
+		System.out.println("Loading mobile details...\n");
+	}
+	
+	@Test(priority=3)
+	void placeProductOrder() {
+		System.out.println("Order placed successfully\n");
+	}
+	
 }
