e8c46812023e363d23937f5749c56d9f5dc6f0db
kaleshripad11 <kaleshripad11@gmail.com>
2023-09-16 13:02:34 +0530

    added testng groups

---
+++ src/test/java/testngdemos/TestNg_Groups.java
@@ -0,0 +1,10 @@
+package testngdemos;
+
+public class TestNg_Groups {
+	/*
+	 * We can group test methods in testng 
+	 * To do this, add parameter "groups" to the "test" annotation
+	 * Syntax : @Test(groups={"Groupname1", "Groupname2",...})
+	 */
+
+}
