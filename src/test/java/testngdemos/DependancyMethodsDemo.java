c6db6b555c93e503c971eeb5d3b112b41cf8950a
kaleshripad11 <kaleshripad11@gmail.com>
2023-09-17 16:18:27 +0530

    Updated file DependancyMethodsDemo.java

--- src/test/java/testngdemos/DependancyMethodsDemo.java
+++ src/test/java/testngdemos/DependancyMethodsDemo.java
@@ -1,6 +1,39 @@
 package testngdemos;
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.*;
 
 public class DependancyMethodsDemo {
+	/*
+	 * In testng methods dependancy can be set using parameter 'dependsOnMethods'
+	 * It's syntax is 'dependsOnMethods={Sequence of dependent methods}'
+	 */
 	
-
+	@Test(priority=1)
+	void openBrowser() {
+		assertEquals(false, true);
+	}
+	
+	@Test(priority=2,dependsOnMethods = {"openBrowser"})
+	void openApp() {
+		/*
+		 * This method is dependant on "openBrowser()". If "openBrowser()" assert fails
+		 */
+		System.out.println("Loading the application...");
+		assertEquals(false, true);
+	}
+	
+	@Test(priority=3,dependsOnMethods = {"openBrowser","openApp"})
+	void loginApp() {
+		System.out.println("Login page...");
+		assertEquals(true, true);
+	}
+	
+	@Test(priority=4,dependsOnMethods= {"loginApp"})
+	void performSearch() {
+		System.out.println("Searching item : Mobiles");
+		System.out.println("Result found : ");
+		assertEquals(true, true);
+	}
+	
 }
